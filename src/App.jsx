93f6005e13d2c3374fac92fa724062a429747ddf
Julius <gwolojulius2@gmail.com>
2024-09-19 15:14:10 +0200

    Program Section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
 
 function App() {
-
   return (
     <>
-     
-     <Navbar/>
-     <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle="Our Program" title="What We Offer" />
+        <Programs />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./Hero.css";
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
     <div className="hero container">
       <div className="hero-text">
-
-      <h1>We Ensure Quality Education For Better World</h1>
-      <p>
-        Our cutting-edge curriculum is designed to empower students with
-        knowledge, skills and experiences required in the dynamic filed  of
-        Education
-      </p>
-      <button className="btn">Explore More</button>
+        <h1>We Ensure Quality Education For Better World</h1>
+        <p>
+          Our cutting-edge curriculum is designed to empower students with
+          knowledge, skills and experiences required in the dynamic filed of
+          Education
+        </p>
+        <button className="btn hero_btn">
+          Explore More <img src={dark_arrow} alt="" />
+        </button>
       </div>
     </div>
   );

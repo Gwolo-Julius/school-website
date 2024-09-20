44126854ea696a91e70928b50af6833152a7cbbf
Julius <gwolojulius2@gmail.com>
2024-09-20 06:33:48 +0200

    Contact form completed successfully

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,8 +4,35 @@ import message from "../../assets/msg-icon.png";
 import mail_icon from "../../assets/mail-icon.png";
 import phone_icon from "../../assets/phone-icon.png";
 import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+
+   const [result, setResult] = React.useState("");
+
+   const onSubmit = async (event) => {
+     event.preventDefault();
+     setResult("Sending....");
+     const formData = new FormData(event.target);
+
+     formData.append("access_key", "31286bca-357f-4715-a206-dc0a3ad70660");
+
+     const response = await fetch("https://api.web3forms.com/submit", {
+       method: "POST",
+       body: formData,
+     });
+
+     const data = await response.json();
+
+     if (data.success) {
+       setResult("Your Message is Submitted Successfully!");
+       event.target.reset();
+     } else {
+       console.log("Error", data);
+       setResult(data.message);
+     }
+   };
+
   return (
     <div className="contact">
       <div className="contact-col">
@@ -32,7 +59,38 @@ const Contact = () => {
           </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <br />
+          <label htmlFor="telephone">Phone:</label>
+          <input
+            type="telephone"
+            name="phone"
+            placeholder="Your Phone Number"
+            required
+          />
+          <br />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            type="textarea"
+            name="message"
+            rows={6}
+            placeholder="Enter Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn form-btn">
+            Submit now <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
